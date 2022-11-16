9f18e76c5ecd655dc4d11da1766b0c34df6c2a03
azer1422 <91677701+azer1422@users.noreply.github.com>
2022-11-16 00:27:40 -0500

    weather app creation

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a52dd9ac78msh485c94d3790b6f1p16fd02jsnf66d182e4d3e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f92a8778fe8bc519709a5c55d7e94025"
